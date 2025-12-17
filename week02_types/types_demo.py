8b1e0f5115d0141f16801b2a08286b539abec52e
YUTA <yutaretr0@gmail.com>
2025-12-17 23:53:08 +0300

    Вторая часть второго задания

--- week02_types/types_demo.py
+++ week02_types/types_demo.py
@@ -1,3 +1,5 @@
+# первая часть задания
+
 my_int = 3
 my_float = 3.5
 my_str = "привет"
@@ -14,3 +16,18 @@ print(my_int)
 my_int += 1
 
 print(my_int)
+
+
+# вторая часть задания
+
+my_list = [1, 2, 3, 4, 5] # список
+my_list.append(6) # добавляем новое значение
+print(id(my_list))
+
+my_dict = {1: 'one', 2: 'two'}
+my_dict[2] = 'no two'
+print(id(my_dict))
+
+my_set = {1, 2, 3, 4, 5}
+my_set.add(6)
+print(id(my_set))
