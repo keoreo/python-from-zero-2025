92debebe7b6bf7a9a3d3a5ab6f8b5634aa2e9741
YUTA <yutaretr0@gmail.com>
2025-12-17 23:33:03 +0300

    Первая часть второго задания

---
+++ week02_types/types_demo.py
@@ -0,0 +1,16 @@
+my_int = 3
+my_float = 3.5
+my_str = "привет"
+my_bool = True
+my_tuple = (10, 20.3)
+
+"""
+my_str[0] = 'X'
+вызовет ошибку изменения не изменяевого объекта
+"""
+
+print(my_int)
+
+my_int += 1
+
+print(my_int)
